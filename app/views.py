5ae2b0ef6d8bda54fb0eff3ba076455f854f5d48
Sherif Siyanbola <sherifsiyanbola@gmail.com>
2019-08-10 10:17:08 +0100

    multiple search filter added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contact
 from django.views.generic import ListView, DetailView
+from django.db.models import Q #Help in multiple filtering
 
 
 # Create your views here.
@@ -29,7 +30,12 @@ class ContactDetailView(DetailView):
 def search(request):
     if request.GET:
         search_term = request.GET['search_term']
-        search_results = Contact.objects.filter(name__icontains=search_term)
+        search_results = Contact.objects.filter(
+            Q(name__icontains=search_term) |
+            Q(email__icontains=search_term) |
+            Q(info__icontains=search_term) |
+            Q(phone__iexact=search_term)
+            )
         context = {
             'search_term': search_term,
             'contacts': search_results
